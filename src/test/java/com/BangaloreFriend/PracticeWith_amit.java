c662fe5f47e4afa026e9d696338fd1a417212a61
Amit <amitsingh2793@gmail.com>
2023-01-28 12:25:29 +0530

    commit by amit1

---
+++ src/test/java/com/BangaloreFriend/PracticeWith_amit.java
@@ -0,0 +1,13 @@
+package com.BangaloreFriend;
+
+import org.testng.annotations.Test;
+
+public class PracticeWith_amit {
+	
+	@Test
+	public void concept()
+	{
+		System.out.println("Good in concept");
+	}
+
+}
